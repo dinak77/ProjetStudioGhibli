0e3283abb2b4c43a7a9be8091a2e5d31e89f562e
Mickaël <mickael@MacBook-Air-de-Mickael.local>
2019-08-05 21:18:20 +0200

    Add Yonebayashi and assets. Modification app-routing, app-module and navbar

---
+++ src/app/yonebayashi/yonebayashi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-yonebayashi',
+  templateUrl: './yonebayashi.component.html',
+  styleUrls: ['./yonebayashi.component.css']
+})
+export class YonebayashiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
